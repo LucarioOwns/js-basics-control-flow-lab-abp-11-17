ba19af466e8c62376bc97d64db9b351b556e9292
Roy Frias <roy.frias2@gmail.com>
2017-11-17 23:14:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(199) {
-  if (scuberGreetingForFeet <= 400) {
-    return "This one is on me!";
+  if (scuberGreetingForFeet <= 400){
+    return "This one is one me!";
   }
-
 }
+  
+  
+
+
